41cd4c4b59f55b03750bf8b01dc669e88dede682
Blake A. Long <blake.a.long@gmail.com>
2020-11-04 14:50:22 -0700

    EmployeeDAO interface, Employee entity, and set JDBC properties

---
+++ src/main/java/com/blakelong/thymeleafcruddb/ThymeleafCrudDbApplication.java
@@ -0,0 +1,13 @@
+package com.blakelong.thymeleafcruddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCrudDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafCrudDbApplication.class, args);
+	}
+
+}
